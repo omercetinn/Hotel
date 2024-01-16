13b752e6d092564285ad3443cbdda9235efd8a47
Ömer Çetin <94043864+omercetinn@users.noreply.github.com>
2024-01-16 11:52:46 +0300

    get handler

---
+++ Core/Hotels.Application/Features/CQRS/Handlers/AboutHandler/GetAboutByIdQueryHandler.cs
@@ -0,0 +1,34 @@
+﻿using Hotels.Application.Features.CQRS.Queries.AboutQueries;
+using Hotels.Application.Features.CQRS.Results.AboutResults;
+using Hotels.Application.Interfaces;
+using Hotels.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotels.Application.Features.CQRS.Handlers.AboutHandler
+{
+    public class GetAboutByIdQueryHandler
+    {
+        private readonly IRepository<About> _repository;
+
+        public GetAboutByIdQueryHandler(IRepository<About> repository)
+        {
+            _repository = repository;
+        }
+        public async Task<GetAboutByIdQueryResult> Handle(GetAboutByIdQuery query)
+        {
+            var values = await _repository.GetByIdAsync(query.Id);
+            return new GetAboutByIdQueryResult
+            {
+                AboutId = values.AboutId,
+                Description = values.Description,
+                ImageUrl = values.ImageUrl,
+                Title = values.Title
+            };
+        }
+    }
+}
---
+++ Core/Hotels.Application/Features/CQRS/Handlers/AboutHandler/GetAboutQueryHandler.cs
@@ -0,0 +1,35 @@
+﻿using Hotels.Application.Features.CQRS.Results.AboutResults;
+using Hotels.Application.Interfaces;
+using Hotels.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotels.Application.Features.CQRS.Handlers.AboutHandler
+{
+    public class GetAboutQueryHandler
+    {
+        private readonly IRepository<About> _repository;
+
+        public GetAboutQueryHandler(IRepository<About> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<List<GetAboutQueryResult>> Handler()
+        {
+            var values = await _repository.GetAllAsync();
+            return values.Select(x => new GetAboutQueryResult
+            {
+                AboutId = x.AboutId,
+                Description = x.Description,
+                Title = x.Title,
+                ImageUrl = x.ImageUrl
+            }).ToList();
+
+        }
+    }
+}
