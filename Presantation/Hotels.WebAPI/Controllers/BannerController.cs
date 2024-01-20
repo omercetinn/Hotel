598510ea4ae8eb8c52a396f65e31090b5111c91e
Ömer Çetin <94043864+omercetinn@users.noreply.github.com>
2024-01-20 13:20:46 +0300

    banner controller

---
+++ Presantation/Hotels.WebAPI/Controllers/BannerController.cs
@@ -0,0 +1,59 @@
+﻿using Hotels.Application.Features.CQRS.Commands.BannerCommands;
+using Hotels.Application.Features.CQRS.Handlers.BannerHandler;
+using Hotels.Application.Features.CQRS.Queries.BannerQueries;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hotels.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BannerController : ControllerBase
+    {
+        private readonly GetBannerQueryHandler _getBannerQueryHandler;
+        private readonly GetBannerByIdQueryHandler _getBannerByIdQueryHandler;
+        private readonly CreateBannerCommandHandler _createBannerCommandHandler;
+        private readonly UpdateBannerCommandHandler _updateBannerCommandHandler;
+        private readonly RemoveBannerCommandHandler _removeBannerCommandHandler;
+
+        public BannerController(GetBannerQueryHandler getBannerQueryHandler, GetBannerByIdQueryHandler getBannerByIdQueryHandler, CreateBannerCommandHandler createBannerCommandHandler, UpdateBannerCommandHandler updateBannerCommandHandler, RemoveBannerCommandHandler removeBannerCommandHandler)
+        {
+            _getBannerQueryHandler = getBannerQueryHandler;
+            _getBannerByIdQueryHandler = getBannerByIdQueryHandler;
+            _createBannerCommandHandler = createBannerCommandHandler;
+            _updateBannerCommandHandler = updateBannerCommandHandler;
+            _removeBannerCommandHandler = removeBannerCommandHandler;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> BannerList()
+        {
+            var values = await _getBannerQueryHandler.Handle();
+            return Ok(values);
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetBanner(int id)
+        {
+            var value = await _getBannerByIdQueryHandler.Handle(new GetBannerByIdQuery(id));
+            return Ok(value);
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateBanner(CreateBannerCommand command)
+        {
+            await _createBannerCommandHandler.Handle(command);
+            return Ok("Banner Eklendi");
+        }
+        [HttpDelete]
+        public async Task<IActionResult> RemoveBanner(int id)
+        {
+            await _removeBannerCommandHandler.Handle(new RemoveBannerCommand(id));
+            return Ok("Banner Silindi");
+        }
+        [HttpPut]
+        public async Task<IActionResult> UpdateBanner(UpdateBannerCommand command)
+        {
+            await _updateBannerCommandHandler.Handle(command);
+            return Ok("Banner Güncellendi");
+        }
+    }
+}
--- Presantation/Hotels.WebAPI/Program.cs
+++ Presantation/Hotels.WebAPI/Program.cs
@@ -1,4 +1,5 @@
 using Hotels.Application.Features.CQRS.Handlers.AboutHandler;
+using Hotels.Application.Features.CQRS.Handlers.BannerHandler;
 using Hotels.Application.Interfaces;
 using Hotels.Persistence.Context;
 using Hotels.Persistence.Repositories;
@@ -14,6 +15,13 @@ builder.Services.AddScoped<GetAboutQueryHandler>();
 builder.Services.AddScoped<CreateAboutCommandHandler>();
 builder.Services.AddScoped<UpdateAboutCommandHandler>();
 builder.Services.AddScoped<RemoveAboutCommandHandler>();
+
+
+builder.Services.AddScoped<GetBannerByIdQueryHandler>();
+builder.Services.AddScoped<GetBannerQueryHandler>();
+builder.Services.AddScoped<CreateBannerCommandHandler>();
+builder.Services.AddScoped<UpdateBannerCommandHandler>();
+builder.Services.AddScoped<RemoveBannerCommandHandler>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
