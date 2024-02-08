3d2f1ac81d74dbaa2f2cb49cf7243af175d64ab4
Ömer Çetin <94043864+omercetinn@users.noreply.github.com>
2024-02-08 13:40:54 +0300

    footeraddress controller

---
+++ Presantation/Hotels.WebAPI/Controllers/FooterAddressController.cs
@@ -0,0 +1,49 @@
+﻿using Hotels.Application.Features.Mediator.Commands.FooterAddressCommands;
+using Hotels.Application.Features.Mediator.Queries.FooterAdressQueries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hotels.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FooterAddressController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public FooterAddressController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        [HttpGet]
+        public async Task<IActionResult> FooterAddressList()
+        {
+            var values = await _mediator.Send(new GetFooterAddressQuery());
+            return Ok(values);
+
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetFooterAddress(int id)
+        {
+            var value = await _mediator.Send(new GetFooterAddressByIdQuery(id));
+            return Ok(value);
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateFooterAddress(CreateFooterAddressCommand command)
+        {
+            await _mediator.Send(command);
+            return Ok("Özellik başarıyla eklendi");
+        }
+        [HttpDelete]
+        public async Task<IActionResult> DeleteFooterAddress(int id)
+        {
+            await _mediator.Send(new RemoveFooterAddressCommand(id));
+            return Ok("Özellik silindi");
+        }
+        [HttpPut]
+        public async Task<IActionResult> UpdateFooterAddress(UpdateFooterAddressCommand command)
+        {
+            await _mediator.Send(command);
+            return Ok("Özellik Güncellendi");
+        }
+    }
+}
