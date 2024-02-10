a90cfe056412d65381154eb620facaeffe9bfd95
Ömer Çetin <94043864+omercetinn@users.noreply.github.com>
2024-02-10 18:32:28 +0300

    fix

--- Presantation/Hotels.WebAPI/Controllers/FooterAddressController.cs
+++ Presantation/Hotels.WebAPI/Controllers/FooterAddressController.cs
@@ -14,6 +14,7 @@ namespace Hotels.WebAPI.Controllers
         {
             _mediator = mediator;
         }
+
         [HttpGet]
         public async Task<IActionResult> FooterAddressList()
         {
